272af5eb361bfc9163c2f05bee0fc31d597a645f
John Carr <john.carr@unrouted.co.uk>
2012-04-09 11:58:01 +0100

    Initial checkin

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+version = '0.0.0'
+
+setup(
+    name = 'buildbot_travis',
+    version = version,
+    description = "Adapt buildbot to work a little more like Travis.",
+    keywords = "buildbot travis ci",
+    author = "John Carr",
+    author_email = "john.carr@unrouted.co.uk",
+    license="Apache Software License",
+    packages = find_packages(exclude=['ez_setup']),
+    include_package_data = True,
+    zip_safe = False,
+    install_requires = [
+        'setuptools',
+        'buildbot',
+    ],
+)
