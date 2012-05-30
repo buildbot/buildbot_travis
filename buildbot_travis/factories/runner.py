c76ecb801c838b3e94eaf99d7b9b0342f4556d7e
John Carr <john.carr@unrouted.co.uk>
2012-05-30 20:00:50 +0100

    Remove traces of TravisRunner

--- buildbot_travis/factories/runner.py
+++ buildbot_travis/factories/runner.py
@@ -1,14 +1,10 @@
 
 from .base import BaseFactory
-from ..steps import TravisRunner, TravisSetupSteps
+from ..steps import TravisSetupSteps
 
 
 class TravisFactory(BaseFactory):
     def __init__(self, repository, vcs_type=None, branch=None, username=None, password=None):
         BaseFactory.__init__(self, repository, vcs_type=vcs_type, branch=branch, username=username, password=password)
-        #for step in ("before_install", "install", "after_install", "before_script", "script", "after_script"):
-        #    self.addStep(TravisRunner(
-        #        step = step,
-        #        ))
         self.addStep(TravisSetupSteps())
 
--- buildbot_travis/steps/__init__.py
+++ buildbot_travis/steps/__init__.py
@@ -1,5 +1,4 @@
 
-from .runner import TravisRunner
 from .spawner import TravisTrigger
 from .create_steps import TravisSetupSteps
 
