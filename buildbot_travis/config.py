a72627d38ed170a9050b35ff147bf49ccea60298
John Carr <john.carr@unrouted.co.uk>
2012-06-06 21:38:44 +0100

    Fixes

--- buildbot_travis/config.py
+++ buildbot_travis/config.py
@@ -1,8 +1,9 @@
+from twisted.python import log
 
 class Sadface:
     brdict = None
 
-def nextBuild(self, builder, requests):
+def nextBuild(builder, requests):
     """
     This nextBuild function stops builds from having a spawner fire up
     if there are no free builders
@@ -12,9 +13,9 @@ def nextBuild(self, builder, requests):
         log.msg("nextBuild: Builder %s does not have a builder_status" % builder)
         return Sadface()
 
-    job = builder.master.getBuilder("%s-job" % builder.name)
+    job = builder.master.getStatus().getBuilder("%s-job" % builder.name)
     for slavename in job.slavenames:
-        slave = builder.master.getSlave(slavename)
+        slave = builder.master.botmaster.slaves[slavename]
         if not slave.canStartBuild():
             continue
         if hasattr(slave, "substantiation_deferred") and slave.substantiation_deferred:
