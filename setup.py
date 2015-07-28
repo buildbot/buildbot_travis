9dc12a8c3cc4294f20e99b6dfe5b4eebee27a90f
Pierre Tardy <pierre.tardy@intel.com>
2015-07-28 15:17:06 +0200

    require buildbot > 0.9.0b1
    this auto enables --pre of pip
    
    Signed-off-by: Pierre Tardy <pierre.tardy@intel.com>

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ setup_www_plugin(
     },
     install_requires=[
         'setuptools',
-        'buildbot',
-        'buildbot-www',
+        'buildbot >= 0.9.0b1',
+        'buildbot-www >= 0.9.0b1',
         'buildbot-slave',
         'klein',
         'PyYAML',
