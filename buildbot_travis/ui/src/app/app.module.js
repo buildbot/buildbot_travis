4f88aac73d876fb91ad03e6611f6a1d24d3c2c67
Pierre Tardy <tardyp@gmail.com>
2020-08-05 19:59:12 +0200

    use webpack for build

--- buildbot_travis/ui/src/app/app.module.js
+++ buildbot_travis/ui/src/app/app.module.js
@@ -10,3 +10,13 @@ class BuildbotTravis {
 
 
 angular.module('app', new BuildbotTravis());
+
+require("./configEditors/directives/input-tags.directive.js");
+require("./configEditors/directives/input_stages.directive.js");
+require("./configEditors/directives/codeeditor.directive.js");
+require("./configEditors/directives/configpage.directive.js");
+require("./configEditors/deploydialog.controller.js");
+require("./configEditors/deploy_latest_dialog.controller.js");
+require("./configEditors/deploy.controller.js");
+require("./configEditors/bbtravis_config.controller.js");
+require("./configEditors/config.route.js");
